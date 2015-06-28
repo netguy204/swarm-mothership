853f54a475c03e991013d7a2b7a2d35930ef5cb0
Brian Taylor <Brian.Taylor@jhuapl.edu>
2015-06-28 13:46:56 -0400

    moving out joystick code

---
+++ pi/joystick.h
@@ -0,0 +1,19 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+#include <asm/types.h>
+
+#define NAXIS 6
+#define NBUTTONS 8
+
+struct js_state {
+  __s16 axis[NAXIS];
+  __u8 button[NBUTTONS];
+};
+
+
+void joystickInit();
+
+void joystickState(js_state *js);
+
+#endif
--- pi/pi2cface.cpp
+++ pi/pi2cface.cpp
@@ -11,78 +11,7 @@
 #include <sys/ioctl.h>
 
 #include "protocol.h"
-
-#define NAXIS 6
-#define NBUTTONS 8
-
-struct js_event {
-  __u32 time;     /* event timestamp in milliseconds */
-  __s16 value;    /* value */
-  __u8 type;      /* event type */
-  __u8 number;    /* axis/button number */
-};
-
-struct js_state {
-  __s16 axis[NAXIS];
-  __u8 button[NBUTTONS];
-};
-
-#define JS_EVENT_BUTTON         0x01    /* button pressed/released */
-#define JS_EVENT_AXIS           0x02    /* joystick moved */
-#define JS_EVENT_INIT           0x80    /* initial state of device */
-
-int jsfd = 0;
-
-void joystickInit() {
-  jsfd = 0;
-}
-
-void joystickState(js_state *js) {
-  static js_state state;
-  static bool initialized = false;
-  if(!initialized) {
-    memset(&state, sizeof(js_state), 0);
-    initialized = true;
-  }
-
-  if(jsfd <= 0) {
-    // handle device reconnect
-    jsfd = open ("/dev/input/js0", O_RDONLY);
-    if(jsfd <= 0) {
-      fprintf(stderr, "Cannot read /dev/input/js0: %s\n", strerror(errno));
-      return;
-    }
-  }
-
-  pollfd pfd;
-  pfd.fd = jsfd;
-  pfd.events = POLLIN;
-        int ret;
-  while(0 < (ret = poll(&pfd, 1, 0))) {
-    js_event event;
-    ret = read(jsfd, &event, sizeof(struct js_event));
-    if(ret < 0) {
-      fprintf(stderr, "js0 read error: %s\n", strerror(errno));
-    }
-    if(ret != sizeof(struct js_event)) {
-      // handle device disconnect
-      close(jsfd);
-      jsfd = 0;
-      return;
-    }
-    event.type &= ~JS_EVENT_INIT;
-    if(event.type == JS_EVENT_BUTTON && event.number < NBUTTONS) {
-      state.button[event.number] = event.value;
-    } else if(event.type == JS_EVENT_AXIS && event.number < NAXIS) {
-      state.axis[event.number] = event.value;
-    }
-  }
-  if(ret < 0) {
-    fprintf(stderr, "js0 event failed: %s\n", strerror(errno));
-  }
-
-  *js = state;
-}
+#include "joystick.h"
 
 // The PiWeather board i2c address
 //#define ADDRESS 0x04
