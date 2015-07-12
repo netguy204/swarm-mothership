61b811202c736351fff07bd771205b3e2010afcf
Liam Echlin <wechlin@umbctraining.com>
2015-07-12 12:22:18 -0400

    Moved FSM methods to correct file

--- pi/wsfsm.cpp
+++ pi/wsfsm.cpp
@@ -127,6 +127,60 @@ void WebServiceFSM::update() {
   if(state == UpstreamState::SENDING && delayExpired()) {
     // to be implemented later, when mothership has sensor status
   }
+}
+
+void WebServiceFSM::putCmdStatus(long cid, bool status) {
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["cid"] = 0; // mothership
+
+  if (status) {
+    root["complete"] = true;
+  } else {
+    root["complete"] = false;
+  }
 
 
+}
+
+
+/*
+cmd json syntax:
+if type=DRIVE speed, heading
+*/
+Message WebServiceFSM::pullQueuedCmd() {
+
+  StaticJsonBuffer<200> jsonBuffer;
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"pid\":0}");
+  CURLcode res = curl_easy_perform(curl);
+
+  if(res != CURLE_OK) {
+    fprintf(stderr, "WebQ: Failed to access %s: %s\n", "localhost",
+        curl_easy_strerror(res));
+    //return 1;
+  }
+  fprintf(stderr, "WebQ: Connection successful\n");
+  fprintf(stderr, "%s\n", cmdFromQueue);
+  JsonObject& root = jsonBuffer.parseObject(cmdFromQueue);
+
+  if (!root.success()) {
+    printf("json parsing failed.\n");
+    // should return?
+  }
+
+  long pid = root["pid"];
+
+  //  long pid = root["properties"]["id"];
+  //  long cmdId = root["cid"]; // UNCOMMENT AFTER ADDED TO WEBSERVICE
+  fprintf(stderr,"pid =  %ld\n",pid);
+  Message msg;
+  // TEMP
+  int8_t speed_ival = 1;
+  int8_t angle_ival = 1;
+  int id = 0;
+  // end TEMP
+  messageSignedInit(&msg, COMMAND_SET_MOTION, speed_ival, angle_ival, id++);
+  return msg;
 }
