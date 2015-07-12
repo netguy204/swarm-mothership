f9304e1efc36d5b77b54672ad94540584e7e1c33
Liam Echlin <wechlin@umbctraining.com>
2015-07-12 14:49:11 -0400

    Added some diagnostic defaults, and allow for a different host via the command-line invocation

--- pi/set_heading.sh
+++ pi/set_heading.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HUNTER=$1
-HEADING=$2
+HUNTER=${1:-10} # Hunter X
+HEADING=${2:-2} # double header
 
-curl -X POST --data "{\"type\": \"SET_HEADING\", \"duration\": 3000, \"heading\": $HEADING, \"pid\":$HUNTER}" http://192.168.168.100:8080/commands --header "Content-Type:application/json"
+curl -X POST --data "{\"type\": \"SET_HEADING\", \"duration\": 3000, \"heading\": $HEADING, \"pid\":$HUNTER}" http://${HOST:-192.168.168.100}:8080/commands --header "Content-Type:application/json"
