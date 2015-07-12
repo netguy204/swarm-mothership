c1ac203b3603056aec1e664e3743fe6c45cbfccb
Liam Echlin <wechlin@umbctraining.com>
2015-07-12 12:34:30 -0400

    Removed unnecessary header

--- pi/wtest.cpp
+++ pi/wtest.cpp
@@ -1,13 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "wsfsm.h"
-#include <curl/curl.h>
 
 int main(int argc, char** argv) {
 
-    long cmd = 1;
-    bool status = true;
+  long cmd = 1;
+  bool status = true;
 
   WebServiceFSM wfsm{"http://localhost:8080/commands"};
   wfsm.putCmdStatus(cmd,status);
